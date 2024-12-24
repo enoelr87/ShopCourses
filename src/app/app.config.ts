e6bd00e120a449a0901c3a2a1cf826f9aad10be4
Enoel Ramirez Rodriguez <enoelr87@gmail.com>
2024-12-24 14:05:28 -0300

    lint changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,6 +13,7 @@ import {
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,9 +25,13 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: false || 'none',
+          darkModeSelector: false,
         },
       },
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
 };
--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
@@ -6,8 +13,27 @@ import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [RouterModule, FloatLabelModule, ButtonModule, InputTextModule],
+  imports: [
+    RouterModule,
+    FloatLabelModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss',
 })
-export default class ForgotPasswordComponent {}
+export default class ForgotPasswordComponent implements OnInit {
+  forgotPasswordForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.forgotPasswordForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  sendUserCode(): void {
+    console.log(this.forgotPasswordForm.value);
+  }
+}
